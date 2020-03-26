b22bc1f9f2eb2b6e71d33d85e02f3a150b439f1c
798831181 <798831181@qq.com>
2020-03-26 15:39:43 +0800

    HystrixOrderMain80 服务降级

--- cloud-consumer-hystrix-order80/src/main/java/com/sqc/cloud/controller/OrderController.java
+++ cloud-consumer-hystrix-order80/src/main/java/com/sqc/cloud/controller/OrderController.java
@@ -1,5 +1,7 @@
 package com.sqc.cloud.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.sqc.cloud.services.PaymentHystrixService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,7 +25,13 @@ public class OrderController {
     }
 
     @GetMapping(value = "/consumer/payment/error")
+    @HystrixCommand(fallbackMethod = "paymentTimeOutFallbackMethod", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1500")
+    })
     public String getPaymentError() {
         return paymentHystrixService.getPaymentError();
+    }
+    public String paymentTimeOutFallbackMethod() {
+        return "我是消费者80,对方支付系统繁忙请10秒种后再试或者自己运行出错请检查自己,o(╥﹏╥)o";
     }
 }
