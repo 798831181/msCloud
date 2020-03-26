39e0bd815a9b3c85e2a2252ffed05f04e8006b45
798831181 <798831181@qq.com>
2020-03-26 17:21:27 +0800

    update PaymentHystrixService.java

--- cloud-consumer-hystrix-order80/src/main/java/com/sqc/cloud/services/PaymentHystrixService.java
+++ cloud-consumer-hystrix-order80/src/main/java/com/sqc/cloud/services/PaymentHystrixService.java
@@ -1,5 +1,6 @@
 package com.sqc.cloud.services;
 
+import com.sqc.cloud.services.impl.PaymentGlobalFallback;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
  * @date 2020-03-26 15:03
  */
 @Component
-@FeignClient(value = "CLOUD-PROVIDER-HYSTRIX-PAYMENT")
+@FeignClient(value = "CLOUD-PROVIDER-HYSTRIX-PAYMENT",fallback = PaymentGlobalFallback.class)
 public interface PaymentHystrixService {
     /**
      * 成功支付
