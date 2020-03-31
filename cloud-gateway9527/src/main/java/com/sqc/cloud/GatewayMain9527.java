423e133dd18e20a2db6247a1e778b3fdf8e17c76
798831181 <798831181@qq.com>
2020-03-31 18:31:40 +0800

    new GatewayMain9527.java

---
+++ cloud-gateway9527/src/main/java/com/sqc/cloud/GatewayMain9527.java
@@ -0,0 +1,17 @@
+package com.sqc.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author sunqichao
+ * @date 2020-03-31 9:07
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayMain9527.class);
+    }
+}
